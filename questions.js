b5ecc30d0a7f4bc47ba2a8eaf8a5be9e1af5969d
Frieder Prinz <frieder@prinz.no>
2019-09-01 16:24:07 +0200

    Added some questions

--- questions.js
+++ questions.js
@@ -1,6 +1,6 @@
 const questions = [
     {
-        prompt: "Was kann man anziehen?",
+        prompt: "Was sind Kleidungsstücke?",
         answers: [
             {
                 correct: false,
@@ -20,6 +20,207 @@ const questions = [
             }
         ],
         points: 10
+    },
+    {
+        prompt: "Wer ist die Person auf dem Bild?",
+        answers: [
+            {
+                correct: true,
+                text: "Manfred"
+            },
+            {
+                correct: true,
+                text: "Koala"
+            },
+            {
+                correct: true,
+                text: "Opa"
+            },
+            {
+                correct: true,
+                text: "Prinz"
+            }
+        ],
+        points: 10
+    },
+    // Secondary for second birthday
+    /*{
+        prompt: "Wer ist die Person auf dem Bild?",
+        answers: [
+            {
+                correct: true,
+                text: "Oma"
+            },
+            {
+                correct: true,
+                text: "Mutti"
+            },
+            {
+                correct: true,
+                text: "Brigitte"
+            },
+            {
+                correct: true,
+                text: "Frau Stenzel"
+            }
+        ],
+        points: 10
+    },*/
+    {
+        prompt: "Mit wie vielen Händen wird das Stück gespielt?",
+        answers: [
+            {
+                correct: false,
+                text: "Freihändig"
+            },
+            {
+                correct: false,
+                text: "Dreihändig"
+            },
+            {
+                correct: true,
+                text: "Zweihändig"
+            },
+            {
+                correct: false,
+                text: "Eihändig"
+            }
+        ],
+        points: 10
+    },
+    {
+        prompt: "Wer hat die KunstWerkstätten in Greifswald gegründet?",
+        answers: [
+            {
+                correct: true,
+                text: "Susanne Prinz"
+            },
+            {
+                correct: false,
+                text: "Ernst Moritz Arndt"
+            },
+            {
+                correct: false,
+                text: "Pommerscher Künstlerbund"
+            },
+            {
+                correct: false,
+                text: "MISSING OPTION"
+            }
+        ],
+        points: 10
+    },
+    // Secondary for second birthday
+    /*{
+        prompt: "Welcher berühmte Baumeister hat das Haus in der Liebermannstr. 10 errichtet?",
+        answers: [
+            {
+                correct: false,
+                text: "Max Liebermann"
+            },
+            {
+                correct: false,
+                text: "Fürst Pückler"
+            },
+            {
+                correct: true,
+                text: "Richard Stenzel"
+            },
+            {
+                correct: false,
+                text: "Santa Klaus"
+            }
+        ],
+        points: 10
+    },*/
+    {
+        prompt: "Wer ist der Gastgeber?",
+        answers: [
+            {
+                correct: false,
+                text: "Der Hase"
+            },
+            {
+                correct: true,
+                text: "Der Igel"
+            },
+            {
+                correct: false,
+                text: "Der Löwe"
+            },
+            {
+                correct: false,
+                text: "OPTION NEEDS IMPROVEMENT"
+            }
+        ],
+        points: 10
+    },
+    {
+        prompt: "Welcher berühmte Künstler hat dieses Bild gemalt?",
+        answers: [
+            {
+                correct: false,
+                text: "Albrecht Dürer"
+            },
+            {
+                correct: false,
+                text: "Mozart"
+            },
+            {
+                correct: true,
+                text: "Manfred Prinz"
+            },
+            {
+                correct: false,
+                text: "Picasso"
+            }
+        ],
+        points: 20
+    },
+    // Secondary for second birthday
+    /*{
+        prompt: "Was sollte die Oma mit Frieder machen?",
+        answers: [
+            {
+                correct: false,
+                text: "Mästen"
+            },
+            {
+                correct: true,
+                text: "Aufpassen"
+            },
+            {
+                correct: false,
+                text: "Beruhigen"
+            },
+            {
+                correct: false,
+                text: "Ignorieren"
+            }
+        ],
+        points: 20
+    },*/
+    {
+        prompt: "Wer hat dieses Video hergestellt?",
+        answers: [
+            {
+                correct: true,
+                text: "Filmwerk statt Prinz"
+            },
+            {
+                correct: true,
+                text: "Die Prinzen - true?"
+            },
+            {
+                correct: false,
+                text: "Filmstudio Babelsberg"
+            },
+            {
+                correct: false,
+                text: "HOLLYWOOD"
+            }
+        ],
+        points: 20
     },
     {
         prompt: "Zwei Dioptrien sind 50cm. Wieviel cm sind 4 Dioptrien?",
